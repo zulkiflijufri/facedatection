0c52a661d75c7d91546f4793d7bc023420f3cf4e
zulkiflijufri <zulkiflijufri54@gmail.com>
2021-01-14 17:45:57 +0800

    testing data

--- testing_detection.py
+++ testing_detection.py
@@ -5,24 +5,42 @@ eyeDetector = cv2.CascadeClassifier("haarcascade_eye.xml")
 noseDetector = cv2.CascadeClassifier("haarcascade_nose.xml")
 mouthDetector = cv2.CascadeClassifier("haarcascade_mouth.xml")
 
+faceRecog = cv2.face.LBPHFaceRecognizer_create()
+faceRecog.read('training/webcam.xml')
+
 camera = cv2.VideoCapture(0)
 
-# camera.set(3, 450)
-# camera.set(4, 300)
+camera.set(3, 640)
+camera.set(4, 480)
 
-webcamId = 1
+peoples = ["Siapa ya?", "Zulkifli"]
+
+minWidht = camera.get(3)
+minHeight = camera.get(4)
 
 while True:
-    frameId, frame = camera.read()
+    _, frame = camera.read()
+    # frame = cv2.flip(frame, 1)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     # Deteksi wajah
     face = faceDetector.detectMultiScale(
         gray,
         scaleFactor = 1.5, # Parameter specifying how much the image size is reduced at each image scale.
         minNeighbors = 3, # Parameter specifying how many neighbors each candidate rectangle should have to retain it.
+        # minSize = (640,480)
     )
     for x,y,w,h in face:
         cv2.rectangle(frame,(x,y),(x+w, y+h),(0,0,255),3)
+        _, confidence  = faceRecog.predict(gray[y:y+h, x:x+w])
+        # print(100 - round(confidence,2))
+        if (confidence <= 50):
+            name = peoples[1]
+            numConfidence = "{conf}%".format(conf = round(confidence,2))
+        else:
+            name = peoples[0]
+            numConfidence = "{conf}%".format(conf = round(confidence,2))
+        cv2.putText(frame, name, (x+w-h+25,y+h-w-50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
+        cv2.putText(frame, numConfidence, (x+w-h+25,y+h-w-15), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
         cv2.imshow("Face Detection", frame)
 
         # Deteksi mata
@@ -57,10 +75,7 @@ while True:
             cv2.rectangle(frame,(x,y),(x+w, y+h),(0, 255, 255),3)
             cv2.imshow("Face Detection", frame)
 
-    cv2.imwrite('webcam/person_'+ str(webcamId) +'.jpg', frame)
-    webcamId+=1
-
-    if cv2.waitKey(1) & 0xFF == ord('q') or cv2.waitKey(1) & webcamId >= 20:
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 camera.release()
